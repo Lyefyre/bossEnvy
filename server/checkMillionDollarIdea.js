d16cf75ac41aa803c1c518b18bdfef106fe1eb3a
Lyefyre <v.schaer@gmx.ch>
2018-11-17 14:44:50 +0000

    Final Version

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,7 +1,7 @@
 const checkMillionDollarIdea = (req, res, next) => {
     const {numWeeks, weeklyRevenue} = req.body;
     const totalSum = Number(numWeeks) * Number(weeklyRevenue);
-    if (!numWeeks || !weeklyRevenue || isNaN(totalSum) || totalSum < 1000000) {
+    if (isNaN(totalSum) || totalSum < 1000000) {
         res.status(400).send();
     } else {
         next();
